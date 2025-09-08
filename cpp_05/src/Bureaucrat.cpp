d16b4fba1a7ab06a612449dfa8c830afdfab42ff
icunha-t <isabeltootill@gmail.com>
2025-09-08 17:18:24 +0100

    ex00 done

---
+++ cpp_05/src/Bureaucrat.cpp
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: icunha-t <icunha-t@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 13:44:26 by icunha-t          #+#    #+#             */
+/*   Updated: 2025/09/08 17:17:44 by icunha-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Bureaucrat.hpp"
+
+
+/******************************************************************************/
+/*                  Constructors, Copy Constructor, Destructor                 */
+/******************************************************************************/
+Bureaucrat::Bureaucrat(): _name("Nameless"), _grade(150){
+	std::cout << BGRN
+			  << "Default Bureaucrat was constructed."
+			  << RES << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade): _name(name){
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+	this->_grade = grade;
+	std::cout << BGRN
+			  << "Bureaucrat with name \""
+			  << name
+			  << "\" and grade \""
+			  << grade
+			  << "\" was constructed."
+			  << RES << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& src): _name(src._name), _grade(src._grade){
+	std::cout << BGRN 
+              << "Bureaucrat was copied and constructed" 
+              << RES << std::endl;
+}
+
+Bureaucrat::~Bureaucrat(){
+	std::cout << BRED 
+              << "Bureaucrat was destroyed" 
+              << RES << std::endl;
+}
+
+/******************************************************************************/
+/*                                Operators                                   */
+/******************************************************************************/
+
+Bureaucrat& Bureaucrat::operator= (const Bureaucrat& src){
+	if (this != &src){
+		this->_grade = src._grade;
+	}
+	std::cout << BYEL 
+              << "Bureaucrat was copied with operator" 
+              << RES << std::endl;
+    return *this;
+}
+
+std::ostream& operator<< (std::ostream& output, const Bureaucrat& b){
+	output << b.getName() << ", bureaucrat grade " << b.getGrade() <<".";
+	return output;
+}
+
+/******************************************************************************/
+/*                              Member Functions                              */
+/******************************************************************************/
+
+const std::string Bureaucrat::getName() const{
+	return this->_name;
+}
+
+int Bureaucrat::getGrade() const{
+	return this->_grade;
+}
+
+void Bureaucrat::incrementGrade(){
+	if (this->_grade <= 1)
+		throw GradeTooHighException();
+	else{
+		std::cout << "incrementing... " << std::endl;
+		this->_grade++;	
+	}	
+}
+
+void Bureaucrat::decrementGrade(){
+	if (this->_grade >= 150)
+		throw GradeTooLowException();
+	else{
+		std::cout << "decrementing... " << std::endl;
+		this->_grade++;	
+	}	
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw(){
+	return "\033[0;31mERROR: grade too high (< 1).\033[0m";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw(){
+	return "\033[0;31mERROR: grade too low (> 150).\033[0m";
+}
---
+++ cpp_05/src/main.cpp
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: icunha-t <icunha-t@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 13:44:01 by icunha-t          #+#    #+#             */
+/*   Updated: 2025/09/08 17:16:12 by icunha-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Bureaucrat.hpp"
+
+int main(){
+	
+	std::cout << std::endl << BCYA
+			  << "*** CREATING A BUREAUCRAT AND USING OVERLOAD OPERATOR << ***"
+			  << RES << std::endl;
+	Bureaucrat isabel("Isabel", 42);
+	std::cout << isabel << std::endl;
+	
+	std::cout << std::endl << BCYA
+			  << "*** TESTING INCREMENT AND DECREMENT ***"
+			  << RES << std::endl;
+	try{
+		isabel.incrementGrade();
+	} catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << isabel << std::endl;
+	try{
+		isabel.decrementGrade();
+	} 
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << isabel << std::endl;
+
+	std::cout << std::endl << BCYA
+			  << "*** USING LIMIT GRADES AND TRYING TO INCREMENT AND DECREMENT ***"
+			  << RES << std::endl;
+	Bureaucrat rodrigo("Rodrigo", 1);
+	std::cout << rodrigo << std::endl;
+	Bureaucrat filipe("filipe", 150);
+	std::cout << filipe << std::endl;
+	try{
+		rodrigo.incrementGrade();
+	} 
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+	try{
+		filipe.decrementGrade();
+	} 
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+	
+	std::cout << std::endl << BCYA
+			  << "*** USING AN INVALID GRADE ***"
+			  << RES << std::endl;
+	try{
+		Bureaucrat philip("PHILIP", 400);
+	} 
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+	
+	std::cout << std::endl << BCYA
+			  << "*** TESTING COPY CONSTRUCTOR - FILIPE INTO JACK ***"
+			  << RES << std::endl;
+	Bureaucrat jack(filipe);
+	std::cout << filipe << std::endl;
+	std::cout << jack << std::endl;
+
+	std::cout << std::endl << BCYA
+			  << "*** TESTING COPY ASSIGNMENT - JACK INTO SANTI ***"
+			  << RES << std::endl;
+	Bureaucrat santi = jack;
+	std::cout << jack << std::endl;
+	std::cout << santi << std::endl;
+
+
+	std::cout << std::endl << BCYA
+			  << "*** SO LONG SU#$&RS ***"
+			  << RES << std::endl;
+	return 0;
+}
