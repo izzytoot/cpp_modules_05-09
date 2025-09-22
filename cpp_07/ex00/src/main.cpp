9cf0b3b9012cf9ff66b3563699e56659096f4a6d
Isabel Tootill <isabeltootill@MacBook-Air-de-Isabel.local>
2025-09-22 20:21:31 +0100

    mid ex01

--- cpp_07/ex00/src/main.cpp
+++ cpp_07/ex00/src/main.cpp
@@ -3,70 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icunha-t <icunha-t@student.42.fr>          +#+  +:+       +#+        */
+/*   By: isabeltootill <isabeltootill@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 18:17:56 by icunha-t          #+#    #+#             */
-/*   Updated: 2025/09/19 15:18:15 by icunha-t         ###   ########.fr       */
+/*   Updated: 2025/09/21 14:21:33 by isabeltooti      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "../inc/Templates.hpp"
 
-// int main(){	
-//     std::cout << std::endl << BCYA << "********* TEST 1 - SWAP *********" << RES << std::endl;
+int main(){	
+    std::cout << std::endl << BCYA << "********* TEST 1 - SWAP *********" << RES << std::endl;
 	
-// 	std::cout << std::endl << BCYA << "* with ints *" << RES << std::endl;
-// 	int x = 5;
-// 	int y = 10;
-// 	std::cout << BYEL << "Before swap: " << RES 
-// 			  << "x = " << x 
-// 			  << "; y = " << y 
-// 			  << std::endl;
-// 	::swap(x, y);
-// 	std::cout << BYEL << "After swap: " << RES 
-// 			  << "x = " << x 
-// 			  << "; y = " << y 
-// 			  << std::endl;
+	std::cout << std::endl << BCYA << "* with ints *" << RES << std::endl;
+	int x = 5;
+	int y = 10;
+	std::cout << BYEL << "Before swap: " << RES 
+			  << "x = " << x 
+			  << "; y = " << y 
+			  << std::endl;
+	::swap(x, y);
+	std::cout << BYEL << "After swap: " << RES 
+			  << "x = " << x 
+			  << "; y = " << y 
+			  << std::endl;
 			  
-// 	std::cout << std::endl << BCYA << "* with chars *" << RES << std::endl;
-// 	char a = 'a';
-// 	char b = 'b';
-// 	std::cout << BYEL << "Before swap: " << RES 
-// 			  << "a = " << a 
-// 			  << "; b = " << b 
-// 			  << std::endl;
-// 	::swap(a, b);
-// 	std::cout << BYEL << "After swap: " << RES 
-// 			  << "a = " << a 
-// 			  << "; b = " << b
-// 			  << std::endl;
+	std::cout << std::endl << BCYA << "* with chars *" << RES << std::endl;
+	char a = 'a';
+	char b = 'b';
+	std::cout << BYEL << "Before swap: " << RES 
+			  << "a = " << a 
+			  << "; b = " << b 
+			  << std::endl;
+	::swap(a, b);
+	std::cout << BYEL << "After swap: " << RES 
+			  << "a = " << a 
+			  << "; b = " << b
+			  << std::endl;
 			  
-// 	std::cout << std::endl << BCYA << "* with strings *" << RES << std::endl;
-// 	std::string str1 = "hello";
-// 	std::string str2 = "goodbye";
-// 	std::cout << BYEL << "Before swap: " << RES 
-// 			  << "str1 = " << str1 
-// 			  << "; str2 = " << str2
-// 			  << std::endl;
-// 	::swap(str1, str2);
-// 	std::cout << BYEL << "After swap: " << RES 
-// 			  << "str1 = " << str1 
-// 			  << "; str2 = " << str2
-// 			  << std::endl;
+	std::cout << std::endl << BCYA << "* with strings *" << RES << std::endl;
+	std::string str1 = "hello";
+	std::string str2 = "goodbye";
+	std::cout << BYEL << "Before swap: " << RES 
+			  << "str1 = " << str1 
+			  << "; str2 = " << str2
+			  << std::endl;
+	::swap(str1, str2);
+	std::cout << BYEL << "After swap: " << RES 
+			  << "str1 = " << str1 
+			  << "; str2 = " << str2
+			  << std::endl;
 
-//     std::cout << std::endl << BCYA << "********* TEST 2 - MIN *********" << RES << std::endl;
-// 	std::cout << BYEL << "values: " << RES << "x = " << x << "; y = " << y << std::endl
-// 			  << BYEL << "min is: " << RES << ::min(x, y)
-// 			  << std::endl;
+    std::cout << std::endl << BCYA << "********* TEST 2 - MIN *********" << RES << std::endl;
+	std::cout << BYEL << "values: " << RES << "x = " << x << "; y = " << y << std::endl
+			  << BYEL << "min is: " << RES << ::min(x, y)
+			  << std::endl;
 			  
-//     std::cout << std::endl << BCYA << "********* TEST 3 - MAX *********" << RES << std::endl;
-// 	std::cout << BYEL << "values: " << RES << "x = " << x << "; y = " << y << std::endl
-// 			  << BYEL << "max is: " << RES << ::max(x, y)
-// 			  << std::endl;
+    std::cout << std::endl << BCYA << "********* TEST 3 - MAX *********" << RES << std::endl;
+	std::cout << BYEL << "values: " << RES << "x = " << x << "; y = " << y << std::endl
+			  << BYEL << "max is: " << RES << ::max(x, y)
+			  << std::endl;
 			  
-// 	return 0;
-// }
+	return 0;
+}
 
 //subject main:
 // int main( void ) {
