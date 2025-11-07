492a7364d713c0ee3399610f3c76e7f3fdcb1155
Isabel Tootill <isabeltootill@MacBook-Air-de-Isabel.local>
2025-11-07 20:29:21 +0000

    mid studying ex03

--- cpp_09/ex02/src/main.cpp
+++ cpp_09/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: isabeltootill <isabeltootill@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 12:39:21 by icunha-t          #+#    #+#             */
-/*   Updated: 2025/11/05 18:42:30 by isabeltooti      ###   ########.fr       */
+/*   Updated: 2025/11/07 20:23:22 by isabeltooti      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main (int ac, char** av){
     try {
         PmergeMe sequence;
         sequence.fillContainers(ac, av);
-        sequence.sort(0);
+        sequence.sort(1);
     } catch(const std::exception& e){
         std::cerr << e.what() << std::endl;
         return 1;
@@ -29,3 +29,8 @@ int main (int ac, char** av){
 
     return 0;
 }
+
+
+/*
+./PmergeMe 11 2 17 0 16 8 6 15 10 3 21 1 18 9 14 19 12 5 4 20 13 7
+*/
